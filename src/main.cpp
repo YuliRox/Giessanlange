e668c266c91cbb2acd61220359ef3b99ca56617b
Sandra Schuhmacher <sandra.schuhmacher@sinc.de>
2024-05-20 19:15:39 +0200

    Correct Pins and debouncing pattern

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,22 @@ Giessanlage anlage; // Default: 24h Interval, 30sek gießen
 unsigned long startTime;
 unsigned long elapsedTime;
 
+#define BUTTON_CLOSED LOW
+#define BUTTON_OPEN HIGH
+#define PUMP_ON LOW
+#define PUMP_OFF HIGH
 #define POTI_PUMP_INTERVAL A2
-#define BUTTON_MANUAL D2
-#define BUTTON_CANCEL D3
+#define BUTTON_MANUAL D3
+#define BUTTON_CANCEL D2
 #define PUMP_RELAIS D4
-#define SWITCH_24H D5
-#define SWITCH_12H D6
+#define SWITCH_12H D5
 
-
-int switch24hState = 0;
-int switch12hState = 0;
-int buttonManualState = 0;
-int buttonCancelState = 0;
+int switch12hState = BUTTON_OPEN;
+int lastbuttonStateInterval = BUTTON_OPEN;
+int buttonManualState = BUTTON_OPEN;
+int lastbuttonStateManual = BUTTON_OPEN;
+int buttonCancelState = BUTTON_OPEN;
+int lastbuttonStateCancel = BUTTON_OPEN;
 int potiPumpInterval = 30UL * 1000UL;
 int minPumpInterval = 5;
 int maxPumpInterval = 60;
@@ -33,9 +37,11 @@ unsigned long outputRemainingWait = 0;
 
 void setup()
 {
-  pinMode(BUTTON_MANUAL, INPUT);
-  pinMode(BUTTON_CANCEL, INPUT);
+  pinMode(BUTTON_MANUAL, INPUT_PULLUP);
+  pinMode(BUTTON_CANCEL, INPUT_PULLUP);
+  pinMode(SWITCH_12H, INPUT_PULLUP);
   pinMode(PUMP_RELAIS, OUTPUT);
+  digitalWrite(PUMP_RELAIS, PUMP_OFF);
 
   Serial.begin(9600);
 
@@ -51,91 +57,114 @@ unsigned long scaleToTime(float reading)
 
 void loop()
 {
-  elapsedTime = micros() - startTime;
-  startTime = micros();
+  unsigned long currentTime = millis();
+  elapsedTime = currentTime - startTime;
+  startTime = currentTime;
   outputRemainingWait += elapsedTime;
-
 
   anlage.tick(elapsedTime);
 
-  if (anlage.isPumping())
-  {
-    digitalWrite(PUMP_RELAIS, LOW);
-    Serial.print("Power to the Pump!");
-  }
-  else
-  {
-    digitalWrite(PUMP_RELAIS, HIGH);
-    Serial.print("Depower the Pump!");
-  }
+    if (anlage.isPumping())
+    {
+      digitalWrite(PUMP_RELAIS, PUMP_ON);
+      Serial.println("Power to the Pump!");
+    }
+    else
+    {
+      digitalWrite(PUMP_RELAIS, PUMP_OFF);
+      //    Serial.println("Depower the Pump!");
+    }
 
   int readingButtonManual = digitalRead(BUTTON_MANUAL);
-  if ((millis() - lastDebounceTimeButtonManual) > debounceDelayMs)
-  {
+    // reset the debouncing timer
+  if (readingButtonManual != lastbuttonStateManual) { lastDebounceTimeButtonManual = currentTime; }
+
+  if ((currentTime - lastDebounceTimeButtonManual) > debounceDelayMs) {
     if (readingButtonManual != buttonManualState)
     {
       buttonManualState = readingButtonManual;
-    }
+      Serial.print("Toggle green Button: ");
+      Serial.println(buttonManualState);
 
-    if (buttonManualState == HIGH)
-    {
-      anlage.triggerPump();
-      Serial.println("Open th floodgates!");
-    }
-  }
-
-  int readingButtonCancel = digitalRead(BUTTON_CANCEL);
-  if ((millis() - lastDebounceTimeButtonCancel) > debounceDelayMs)
-  {
-    if (readingButtonCancel != buttonCancelState)
-    {
-      buttonCancelState = readingButtonCancel;
-    }
-
-    if (buttonCancelState == HIGH)
-    {
-      anlage.stopPump();
-      Serial.println("Cancel!");
-    }
-  }
-
-  int readingIntervalSwitch = digitalRead(SWITCH_12H);
-  if((millis() - lastDebounceTimeIntervalSwitch) > debounceDelayMs){
-    if (readingIntervalSwitch != switch12hState) {
-      switch12hState = readingIntervalSwitch;
-
-      if (switch12hState == HIGH){
-        anlage.setWateringInterval(Giessanlage::INTERVAL_12H);
-        Serial.println("New Interval: 12h");
-      } else {
-        anlage.setWateringInterval(Giessanlage::INTERVAL_24H);
-        Serial.println("New Interval: 24h");
+      if (buttonManualState == BUTTON_CLOSED)
+      {
+        anlage.triggerPump();
+        Serial.println("Open the floodgates!");
       }
-
-      anlage.resetWateringTimer();
     }
   }
 
-  int readingPotiPumpTime = analogRead(POTI_PUMP_INTERVAL);
-  if (currentPotiPumpTime != readingPotiPumpTime){
-    currentPotiPumpTime = readingPotiPumpTime;
-    unsigned long potiPumpInterval = scaleToTime(readingPotiPumpTime);
-    anlage.setPumpTime(potiPumpInterval);
-    Serial.print("New Watering Time: ");
-    Serial.println(potiPumpInterval/1000);
-  }
+  lastbuttonStateManual = readingButtonManual;
 
+    int readingButtonCancel = digitalRead(BUTTON_CANCEL);
+    if(readingButtonCancel != lastbuttonStateCancel) { lastDebounceTimeButtonCancel = currentTime;}
+    if ((currentTime - lastDebounceTimeButtonCancel) > debounceDelayMs)
+    {
+      if (readingButtonCancel != buttonCancelState)
+      {
+        buttonCancelState = readingButtonCancel;
+        Serial.print("Toggle red Button: ");
+        Serial.println(buttonCancelState);
+        
+        if (buttonCancelState == BUTTON_CLOSED)
+        {
+          anlage.stopPump();
+          Serial.println("Cancel!");
+        }
+      }
+    }
 
-  if (outputRemainingWait >= outputRemainingWaitInterval ){
+    lastbuttonStateCancel = readingButtonCancel;
 
-    if (anlage.getState() == Giessanlage::State::Idle){
+    int readingIntervalSwitch = digitalRead(SWITCH_12H);
+    if (readingIntervalSwitch != lastbuttonStateInterval) { lastDebounceTimeIntervalSwitch = currentTime; }
+    if ((currentTime - lastDebounceTimeIntervalSwitch) > debounceDelayMs)
+    {
+      if (readingIntervalSwitch != switch12hState)
+      {
+        switch12hState = readingIntervalSwitch;
+
+        if (switch12hState == HIGH)
+        {
+          anlage.setWateringInterval(Giessanlage::INTERVAL_12H);
+          Serial.println("New Interval: 12h");
+        }
+        else
+        {
+          anlage.setWateringInterval(Giessanlage::INTERVAL_24H);
+          Serial.println("New Interval: 24h");
+        }
+
+        anlage.resetWateringTimer();
+      }
+    }
+
+    lastbuttonStateInterval = readingIntervalSwitch;
+
+    int readingPotiPumpTime = analogRead(POTI_PUMP_INTERVAL);
+    if (currentPotiPumpTime != readingPotiPumpTime)
+    {
+      currentPotiPumpTime = readingPotiPumpTime;
+      unsigned long potiPumpInterval = scaleToTime(readingPotiPumpTime);
+      if (anlage.getPumpTime() != potiPumpInterval)
+      {
+        anlage.setPumpTime(potiPumpInterval);
+        Serial.print("New Watering Time: ");
+        Serial.println(potiPumpInterval / 1000);
+      }
+    }
+
+  if (outputRemainingWait >= outputRemainingWaitInterval)
+  {
+
+    if (anlage.getState() == Giessanlage::State::Idle)
+    {
       unsigned long remainTime = anlage.getRemainingWateringInterval();
       Serial.print("Remaining Time until next waterin occurs: ");
-      Serial.print(remainTime/1000/60);
+      Serial.print(remainTime / 1000UL / 60UL);
       Serial.println("min");
     }
 
     outputRemainingWait = 0;
   }
-
 }
