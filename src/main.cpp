244dd06e9833e82e3ae8f016e3098a6578b93edf
Sandra Schuhmacher <sandra.schuhmacher@sinc.de>
2024-05-20 14:56:26 +0200

    feat: Include buttons, use giesanlage lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
+#include "Giessanlage.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Giessanlage anlage; // Default: 24h Interval, 30sek gießen
+unsigned long startTime;
+unsigned long elapsedTime;
 
-void setup() {
+#define PUMP_RELAIS = D4;
+#define POTI_PUMP_INTERVAL = A2;
+#define POTI_WAIT_INTERVAL = A3;
+#define BUTTON_MANUAL = D2;
+#define BUTTON_CANCEL = D3;
+
+int buttonManualState = 0;
+int buttonCancelState = 0;
+unsigned long lastDebounceTimeButtonManual = 0;
+unsigned long lastDebounceTimeButtonCancel = 0;
+unsigned long debounceDelayMs = 50;
+
+void setup()
+{
+  pinMode(BUTTON_MANUAL, INPUT);
+  pinMode(BUTTON_CANCEL, INPUT);
+  pinMode(PUMP_RELAIS, OUTPUT);
+
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  startTime = micros();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  elapsedTime = micros() - startTime;
+  startTime = micros();
+
+  anlage.tick(elapsedTime);
+
+  if (anlage.isPumping())
+  {
+    digitalWrite(PUMP_RELAIS, LOW);
+  }
+  else
+  {
+    digitalWrite(PUMP_RELAIS, HIGH);
+  }
+
+  int readingButtonManual = digitalRead(BUTTON_MANUAL);
+
+  if ((millis() - lastDebounceTimeButtonManual) > debounceDelayMs)
+  {
+    if (readingButtonManual != buttonManualState)
+    {
+      buttonManualState = readingButtonManual;
+    }
+
+    if(buttonManualState == HIGH){
+      anlage.triggerPump();
+    }
+  }
+
+  int readingButtonCancel = digitalRead(BUTTON_CANCEL);
+  if ((millis() - lastDebounceTimeButtonCancel) > debounceDelayMs)
+  {
+    if (readingButtonCancel != buttonCancelState)
+    {
+      buttonCancelState = readingButtonCancel;
+    }
+
+    if (buttonCancelState == HIGH)
+    {
+      anlage.stopPump();
+    }
+  }
+
+  if (poti1.IsRotated())
+  {
+    // Wie lange pumpen
+    anlage.setPumpTime(poti1.Value * 0.5);
+  }
+
+  if (poti2.IsRotated())
+  {
+    // Wie lange zwischen 2x pumpen warten
+    anlage.setWateringInterval(poti2.Value * 0.5);
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
